b7ba603a129e6a88064dcaa06973a6825c3d9ad9
Marc S <93371079+a20marsimsim@users.noreply.github.com>
2025-02-27 20:01:43 +0100

    Update dirbusting.sh

--- ataque/dirbusting.sh
+++ ataque/dirbusting.sh
@@ -1,5 +1,5 @@
-// Con este comando podemos encontrar directorios y archivos "ocultos" de una página web a base de un ataque de diccionario
+# Con este comando podemos encontrar directorios y archivos "ocultos" de una página web a base de un ataque de diccionario
 dirb https://dominio.com /usr/share/wordlists/dirb/common.txt
 
-// Con este comando podemos encontrar archivos o incluso directorios "ocultos" con extensiones específicas
+# Con este comando podemos encontrar archivos o incluso directorios "ocultos" con extensiones específicas
 dirb https://dominio.com /usr/share/wordlists/dirb/common.txt -X .php,.html,.txt
