19b789777c5e2cdb0965db6085ddc3a2bd32cc76
Marc S <93371079+a20marsimsim@users.noreply.github.com>
2025-02-27 19:01:19 +0100

    Create vulnerable_rce_fix.php

---
+++ defensa/vulnerable_rce_fix.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_GET['cmd'])) {
+    $allowed_commands = ['ls', 'whoami', 'uptime']; // Lista blanca de comandos permitidos
+    $cmd = $_GET['cmd'];
+
+    if (in_array($cmd, $allowed_commands)) {
+        $output = shell_exec(escapeshellcmd($cmd));
+        echo "<pre>" . htmlspecialchars($output, ENT_QUOTES, 'UTF-8') . "</pre>";
+    } else {
+        echo "Comando no permitido.";
+    }
+}
+?>
